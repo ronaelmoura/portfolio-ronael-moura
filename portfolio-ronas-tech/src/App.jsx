53ecc55f7a659890a506c15774bd924b2916dedd
Ronael Moura <ronaelmoura240@gmail.com>
2026-09-07 15:26:31 -0300

    fix: refine action arrow icon

--- portfolio-ronas-tech/src/App.jsx
+++ portfolio-ronas-tech/src/App.jsx
@@ -94,7 +94,7 @@ const socialLinks = [
 
 function Icon({ name, size = 18 }) {
   const paths = {
-    arrow: <><path d="M5 19 19 5" /><path d="M8 5h11v11" /></>,
+    arrow: <><path d="M4 12h15" /><path d="m13 6 6 6-6 6" /></>,
     external: <><path d="M14 5h5v5" /><path d="m19 5-8 8" /><path d="M18 13v5a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h5" /></>,
     github: <><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3.2-.4 6.5-1.6 6.5-7A5.4 5.4 0 0 0 19 3.8 5 5 0 0 0 18.9 1S17.7.6 15 2.4a13.4 13.4 0 0 0-7 0C5.3.6 4.1 1 4.1 1A5 5 0 0 0 4 3.8 5.4 5.4 0 0 0 2.5 7.5c0 5.4 3.3 6.6 6.5 7A4.8 4.8 0 0 0 8 18v4" /><path d="M8 17c-3 .9-3-1.4-4.2-1.8" /></>,
     linkedin: <><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-4 0v7h-4v-7a6 6 0 0 1 6-6Z" /><path d="M2 9h4v12H2z" /><path d="M4 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" /></>,
